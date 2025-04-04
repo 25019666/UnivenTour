236a7f25dcc5cc50872d316000ecf1dba0db3f42
25019666 <25019666@mvula.univen.ac.za>
2025-04-04 11:26:20 +0200

    Add files via upload

---
+++ UnivenTour.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.univentour;
+
+/**
+ *
+ * @author SHONISANI
+ */
+public class UnivenTour {
+    public static void welcome (){
+    System.out.print("Welcome to Univen");
+    }
+    public static void main(String[] args) {
+        UnivenTour.welcome();
+    }
+}
